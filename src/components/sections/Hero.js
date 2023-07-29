c61682476f53630c7de8a447436d1c6868888e94
xdplansprojetos <xdplansprojetos@gmail.com>
2023-07-29 01:31:26 -0300

    kGesso Site

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -84,7 +84,7 @@ const Hero = ({
           </div>
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
             <a
-              data-video="https://player.vimeo.com/video/174002812"
+              data-video="https://www.youtube.com/embed/epogmTkHaKw"
               href="#0"
               aria-controls="video-modal"
               onClick={openModal}
@@ -101,7 +101,7 @@ const Hero = ({
             id="video-modal"
             show={videoModalActive}
             handleClose={closeModal}
-            video="https://player.vimeo.com/video/174002812"
+            video="https://www.youtube.com/embed/epogmTkHaKw"
             videoTag="iframe" />
         </div>
       </div>
